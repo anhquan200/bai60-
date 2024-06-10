d6a54e87bb777c7d012e5f08fa3d1ebce79fbd3b
anhquan200 <166981767+anhquan200@users.noreply.github.com>
2024-06-10 20:13:32 +0700

    btl.js

---
+++ btl.js
@@ -0,0 +1,13 @@
+// Menu
+const menu = document.querySelector(".navbar__links");
+const menuButton = document.querySelector(".navbar__icons");
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+});
+
+overlay.addEventListener("click", () => {
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+});
